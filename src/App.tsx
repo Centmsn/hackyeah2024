0c1aa8dcdff4d25a3b6841b1a6028d18a4fd117b
Michal Rygorowicz <mitsugi23@hotmail.com>
2024-09-28 16:01:02 +0200

    add recharts

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button } from "@/components/ui/button";
+import { RadarChart } from "@/components/radar-chart";
+import { ChartConfig } from "./components/ui/chart";
 
 function App() {
   const [count, setCount] = useState(0);
