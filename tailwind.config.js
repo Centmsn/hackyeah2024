c438364223465083f0e48aa510189af8dda4a748
Michal Rygorowicz <mitsugi23@hotmail.com>
2024-09-29 05:32:11 +0200

    add learning page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import plugin from 'tailwindcss/plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -7,14 +9,43 @@ export default {
     darkMode: "class",
     theme: {
         extend: {
+            colors: {
+                background: "#222",
+                "green-custom": "#00ff91",
+                "green-custom-light": "#ebfff5",
+                "blue-custom": "#1139ff",
+            },
             borderRadius: {
                 lg: 'var(--radius)',
                 md: 'calc(var(--radius) - 2px)',
                 sm: 'calc(var(--radius) - 4px)'
             },
-            colors: {}
+            transitionProperty: {
+                'r': 'r',
+                'stroke-width': 'stroke-width',
+                'fill': 'fill',
+            },
         }
     },
-    plugins: [require("tailwindcss-animate")],
+    plugins: [
+        require("tailwindcss-animate"),
+        plugin(function ({ addUtilities }) {
+            addUtilities({
+                ".scrollbar-none": {
+                    "scrollbar-width": "none",
+                    "&::-webkit-scrollbar": {
+                        display: "none",
+                    },
+                },
+            });
+        }),
+    ],
+    variants: {
+        extend: {
+            r: ['hover', 'focus'],
+            strokeWidth: ['hover', 'focus'],
+            fill: ['hover', 'focus'],
+        },
+    },
 }
 
